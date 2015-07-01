4ba82b3335e94f980ca1230bbaae9718f21344f1
shuin3180 <shuin3180@naver.com>
2015-07-02 05:57:08 +0900

    Documentation codes

--- src/com/d/activity/WebBatteryActivity.java
+++ src/com/d/activity/WebBatteryActivity.java
@@ -16,12 +16,27 @@ import android.os.Bundle;
 import android.view.ViewGroup.LayoutParams;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
+
 import com.d.localdb.BatteryRecord;
 import com.d.localdb.LocalDB;
 import com.google.gson.Gson;
 
+
+/**
+ * 
+ * @author HyunIlHarry
+ * 기능 : BatteryControllerActivity의 그래프 부분을 담당하는 Activity. BatteryControllerActivity에서 상속하여 사용한다.
+ *  
+ *
+ */
 public class WebBatteryActivity extends Activity {
-   
+	/**
+	    * @deprecated
+	    * 사실상 쓰이지 않는 메소드. html파일을 읽어올 때 쓰려 했으니 굳이 쓸 필요가 없어서 안쓰고 있다.
+	    * @param path
+	    * @return
+	    * @throws IOException
+	    */
     public String getAssetAsString(String path) throws IOException {
         StringBuilder buf = new StringBuilder();
         InputStream json;
@@ -39,13 +54,27 @@ public class WebBatteryActivity extends Activity {
         in.close();
         return buf.toString();
     }
-    
+
+    /**
+     * y축의 이름. 그래프에 표시됨
+     */
     public static String yaxisName;
+    /**
+     * 
+     * @author HyunIlHarry
+     * 기능 : 그래프에 들어가는 정보들 (x축, y축, 그래프별 정보)
+     */
     public static class GraphRow {
         public String date;
         public float percentage;
         public float temperature;
     }
+    /**
+     * 
+     * @author HyunIlHarry
+     * 기능 : asset(html 코드)에 넘길 정보를 담는 구조체이다.
+     *
+     */
     public static class Information {
         public String yaxisDesc;
         Information(){
@@ -54,6 +83,13 @@ public class WebBatteryActivity extends Activity {
         ArrayList<GraphRow> data = new ArrayList<WebBatteryActivity.GraphRow>();
     }
     protected SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+    /**
+     * 
+     * @author HyunIlHarry
+     * 기능 : 직접적으로 Information 클래스를 asset의 html코드에 넘기는 클래스
+     * html에서는 Android.info()를 통해서 정보를 받아들인다
+     *
+     */
     public class JSInterface {
         Information info;
         public JSInterface () {
@@ -75,7 +111,11 @@ public class WebBatteryActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 	}
-	
+	/**
+	 * LocalDB에서 BatteryRecord(battery관련 기록)를 긁어온다음 이를 GraphRow 클래스에 넣는 함수
+	 * JSInterface 클래스 생성시 호출됨
+	 * @return Information info
+	 */
 	protected Information getInformation() {
         Information info = new Information();
         Calendar cal = Calendar.getInstance();
--- src/com/d/activity/WebMemoryActivity.java
+++ src/com/d/activity/WebMemoryActivity.java
@@ -22,8 +22,21 @@ import com.d.localdb.LocalDB;
 import com.d.localdb.MemoryRecord;
 import com.google.gson.Gson;
 
+
+/**
+ * 
+ * @author HyunIlHarryShin
+ * 기능 : MemoryActivity의 그래프 부분을 담당하는 Activity. MemoryActivity에서 상속하여 사용한다.
+ *
+ */
 public class WebMemoryActivity extends Activity {
-   
+   /**
+    * @deprecated
+    * 사실상 쓰이지 않는 메소드. html파일을 읽어올 때 쓰려 했으니 굳이 쓸 필요가 없어서 안쓰고 있다.
+    * @param path
+    * @return
+    * @throws IOException
+    */
     public String getAssetAsString(String path) throws IOException {
         StringBuilder buf = new StringBuilder();
         InputStream json;
@@ -42,11 +55,25 @@ public class WebMemoryActivity extends Activity {
         return buf.toString();
     }
     
+    /**
+     * y축의 이름. 그래프에 표시됨
+     */
     public static String yaxisName;
+    /**
+     * 
+     * @author HyunIlHarry
+     * 기능 : 그래프에 들어가는 정보들 (x축, y축, 그래프별 정보)
+     */
     public static class GraphRow {
         public String date;
         public float percentageUsage;
     }
+    /**
+     * 
+     * @author HyunIlHarry
+     * 기능 : asset(html 코드)에 넘길 정보를 담는 구조체이다.
+     *
+     */
     public static class Information {
         public String yaxisDesc;
         Information(){
@@ -55,6 +82,14 @@ public class WebMemoryActivity extends Activity {
         ArrayList<GraphRow> data = new ArrayList<WebMemoryActivity.GraphRow>();
     }
     protected SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+    
+    /**
+     * 
+     * @author HyunIlHarry
+     * 기능 : 직접적으로 Information 클래스를 asset의 html코드에 넘기는 클래스
+     * html에서는 Android.info()를 통해서 정보를 받아들인다
+     *
+     */
     public class JSInterface {
         Information info;
         public JSInterface () {
@@ -77,6 +112,11 @@ public class WebMemoryActivity extends Activity {
 		
 	}
 	
+	/**
+	 * LocalDB에서 DataUsageRecord(Data 사용량 관련 기록)를 긁어온다음 이를 GraphRow 클래스에 넣는 함수
+	 * JSInterface 클래스 생성시 호출됨
+	 * @return Information info
+	 */
 	protected Information getInformation() {
         Information info = new Information();
         Calendar cal = Calendar.getInstance();
